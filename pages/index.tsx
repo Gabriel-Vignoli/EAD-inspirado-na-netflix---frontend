187876c170c131ffe8ec819e42128c0993cf3768
Gabriel Vignoli <vignoligabriel052@gmail.com>
2024-05-04 19:46:09 -0300

    Criação do header da pagina homeNoAuth

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,19 @@
 import Head from "next/head";
 import styles from "../styles/homeNoAuth.module.scss" 
+import HeaderNoAuth from "@/components/homeNoAuth/headerNoAuth";
 
 const HomeNotAuth = function () {
   return (
     <>
       <Head>
-        <title>Home</title>
+        <title>Onebitflix</title>
+        <link rel="shortcut icon" href="/favicon.svg" type="image/x-icon" />
+        <meta property="og:title" content="Onebitflix" key="title"/>
+        <meta name="description" content="Tenha acesso aos melhores conteúdos de programação de uma forma simples e fácil"/>
       </Head>
-      <main></main>
+      <main>
+        <HeaderNoAuth></HeaderNoAuth>
+      </main>
     </>
   );
 };
