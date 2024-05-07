ddb0ab02752093a724aead311d534b03cc977eb1
Gabriel Vignoli <vignoligabriel052@gmail.com>
2024-05-07 20:33:55 -0300

    Finalizando seção dos slides, codigo,conexão com o back-end e rotas, visual e também a responsividade - Seção dos slides

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,9 +2,18 @@ import Head from "next/head";
 import styles from "../styles/HomeNoAuth.module.scss" 
 import HeaderNoAuth from "@/components/homeNoAuth/headerNoAuth";
 import PresentationSection from "@/components/homeNoAuth/presentationSection";
+import SlideSection from "@/components/homeNoAuth/slideSection";
+import { GetStaticProps } from "next";
+import courseService, { CourseType } from "@/services/courseService";
+import { ReactNode } from "react";
 import CardSection from "@/components/homeNoAuth/cardSection";
 
-const HomeNotAuth = function () {
+interface IndexPageProps {
+  children?: ReactNode;
+  course: CourseType[];
+}
+
+const homeNoAuth = function ({ course }: IndexPageProps) {
   return (
     <>
       <Head>
@@ -14,14 +23,26 @@ const HomeNotAuth = function () {
         <meta name="description" content="Tenha acesso aos melhores conteúdos de programação de uma forma simples e fácil"/>
       </Head>
       <main>
-      <div className={styles.sectionBackground}>
-        <HeaderNoAuth></HeaderNoAuth>
-        <PresentationSection></PresentationSection>
-       </div>
-       <CardSection></CardSection>
-      </main>
+		<div className={styles.sectionBackground}>
+			<HeaderNoAuth />
+			<PresentationSection />
+		</div>
+		<CardSection />
+		<SlideSection newestCourses={course}/>
+	</main>
     </>
+
   );
 };
 
-export default HomeNotAuth;
+export const getStaticProps: GetStaticProps = async () => {
+  const res = await courseService.getNewestCourses();
+  return {
+    props: {
+      course: res.data,
+    },
+    revalidate: 3600 * 24,
+  };
+};
+
+export default homeNoAuth;
---
+++ src/components/homeNoAuth/slideSection/index.tsx
@@ -0,0 +1,23 @@
+import { CourseType } from "../../../services/courseService";
+import styles from "./styles.module.scss";
+import { Button, Container } from "reactstrap";
+import SlideComponent from "@/components/common/slideComponent";
+import Link from "next/link";
+
+interface Props {
+    newestCourses: CourseType[]; // Ensure newestCourses is an array of CourseType
+}
+
+const SlideSection = ({ newestCourses }: Props) => {
+    return (
+        <Container className="d-flex flex-column align-items-center">
+            <p className={styles.sectionTitle}>AULAS JÁ DISPONÍVEIS</p>
+            <SlideComponent course={newestCourses} />
+            <Link href="/register">
+                <Button outline color="light" className={styles.slideSectionBtn}>Se cadastre para acessar!</Button>
+            </Link>
+        </Container>
+    );
+};
+
+export default SlideSection;
